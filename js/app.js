0e12fe2fd6cfd8683d6017c133677ab1b28912d9
“somm12” <sominan8744@gmail.com>
2022-08-03 20:52:31 +0900

    newtodolist editing complete next: css checkbox & clear todos

--- js/app.js
+++ js/app.js
@@ -10,15 +10,54 @@ function deleteToDo(event) {
   li.remove();
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   localStorage.setItem(TODOS, JSON.stringify(toDos));
-
-  // const element = document.querySelector();
 }
 function completeToDo(event) {
   const btn = event.target;
   const text = btn.nextSibling;
   text.classList.toggle("complete-todo");
 }
-function editToDo(event) {}
+function updateToDo(text, id) {
+  //front side update
+  console.log(text, id);
+  toDos.map((li) => {
+    if (li.id === id) {
+      li.text = text;
+    }
+  });
+  console.log(toDos);
+  localStorage.setItem(TODOS, JSON.stringify(toDos));
+
+  //localStroage update
+}
+
+function dbToDo(event) {
+  //ToDo text를 더블클릭 했을 시
+  const todoText = event.target;
+  const todoBox = todoText.parentNode;
+  const listToEdit = todoText.parentNode.parentNode;
+  const editingInput = todoText.parentNode.nextSibling;
+  editingInput.value = todoText.innerText;
+  editingInput.classList.toggle("editing");
+  todoBox.classList.toggle("todo-each-box");
+  editingInput.focus();
+
+  document.addEventListener(
+    "click",
+    () => {
+      editingInput.classList.toggle("editing");
+      todoBox.classList.toggle("todo-each-box");
+    },
+    { once: true }
+  );
+  editingInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      todoText.innerText = e.target.value;
+      updateToDo(e.target.value, parseInt(listToEdit.id));
+      editingInput.classList.toggle("editing");
+      todoBox.classList.toggle("todo-each-box");
+    }
+  });
+}
 
 function paintingToDos(text) {
   const todoBox = document.querySelector(".todo-list");
@@ -27,26 +66,27 @@ function paintingToDos(text) {
   const todoListEach = document.createElement("li");
   todoListEach.id = toDos.length + 1;
   const todoEachContent = document.createElement("div");
+
   //화면상에 나타낼 부분
   const todoCompleteBtn = document.createElement("button");
   todoCompleteBtn.innerText = "✅";
   const todoEachText = document.createElement("h3");
   todoEachText.classList.add("todo-text");
   todoEachText.innerText = text;
-  const todoEditBtn = document.createElement("button");
-  todoEditBtn.innerText = "✏️";
+
+  const todoEditInput = document.createElement("input");
+  todoEditInput.classList.add("editing", "editing-box");
   const todoDeleteBtn = document.createElement("button");
   todoDeleteBtn.innerText = "❌";
   //화면상에 나타낼 부분
-  todoEachContent.append(
-    ...[todoCompleteBtn, todoEachText, todoEditBtn, todoDeleteBtn]
-  );
+  todoEachContent.append(...[todoCompleteBtn, todoEachText, todoDeleteBtn]);
 
-  todoListEach.appendChild(todoEachContent);
+  todoListEach.append(...[todoEachContent, todoEditInput]);
   todoList.appendChild(todoListEach);
 
   todoDeleteBtn.addEventListener("click", deleteToDo);
   todoCompleteBtn.addEventListener("click", completeToDo);
+  todoEachText.ondblclick = dbToDo;
 
   const newId = toDos.length + 1;
   const newToDo = {
